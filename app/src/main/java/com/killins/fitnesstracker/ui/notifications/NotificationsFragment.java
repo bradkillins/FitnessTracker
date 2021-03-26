de8bdfd43c30039a049fb7031efe89cf9bf2de4d
bradkillins <bradkillins@gmail.com>
2021-03-25 23:52:12 -0600

    first commit with mostly default bottomNav template

---
+++ app/src/main/java/com/killins/fitnesstracker/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,30 @@
+package com.killins.fitnesstracker.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.killins.fitnesstracker.R;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+}
