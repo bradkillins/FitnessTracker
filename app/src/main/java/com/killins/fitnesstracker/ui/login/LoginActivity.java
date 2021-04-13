ee399bb4f73884b3b86ec570386eac7f5838b635
Adrian <a.todd834@mybvc.ca>
2021-04-13 00:02:28 -0600

    login init

---
+++ app/src/main/java/com/killins/fitnesstracker/ui/login/LoginActivity.java
@@ -0,0 +1,124 @@
+package com.killins.fitnesstracker.ui.login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.killins.fitnesstracker.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+    private LoginViewModel loginViewModel;
+
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button loginButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginViewModel = new ViewModelProvider(this, new LoginViewModel.Factory(getApplicationContext())).get(LoginViewModel.class);
+        usernameText = findViewById(R.id.input_username);
+        passwordText = findViewById(R.id.input_password);
+        loginButton = findViewById(R.id.btn_login);
+        TextView signupLink = findViewById(R.id.link_signup);
+
+        loginButton.setOnClickListener(v -> login());
+
+        signupLink.setOnClickListener(v -> {
+            // Start the Signup activity
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            startActivityForResult(intent, REQUEST_SIGNUP);
+            finish();
+            overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        loginButton.setEnabled(false);
+
+        String email = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        // TODO:
+        //  Fix authentication threading
+        //  Hash passwords? Right now passwords are just stored in db as plaintext
+
+        boolean isAuthenticated = loginViewModel.checkValidLogin(email, password);
+        if(isAuthenticated){
+            Toast.makeText(getBaseContext(), "User Authenticated!", Toast.LENGTH_LONG).show();
+            onLoginSuccess();
+        }else{
+            onLoginFailed();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SIGNUP && resultCode == RESULT_OK) {
+                // TODO: Decide what to do after successful sign up.  Make users log in or navigate directly to the app?
+                Toast.makeText(getBaseContext(), "User Successfully Created! Please Log In.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        loginButton.setEnabled(true);
+        getSharedPreferences("LOGINPREFERENCE", MODE_PRIVATE).edit().putBoolean("signIn", false).apply();
+        Toast.makeText(getBaseContext(), "Successfully Logged In!", Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty()) {
+            usernameText.setError("Enter a valid username!");
+            valid = false;
+        } else {
+            usernameText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            passwordText.setError("Password must be at least 6 characters long!");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return valid;
+    }
+}
---
+++ app/src/main/java/com/killins/fitnesstracker/ui/login/SignupActivity.java
@@ -0,0 +1,137 @@
+package com.killins.fitnesstracker.ui.login;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.killins.fitnesstracker.R;
+
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+    private LoginViewModel loginViewModel;
+
+    EditText userNameText;
+    EditText nameText;
+    EditText emailText;
+    EditText passwordText;
+    EditText reEnterPasswordText;
+    Button signupButton;
+    TextView loginLink;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        loginViewModel = new ViewModelProvider(this, new LoginViewModel.Factory(getApplicationContext())).get(LoginViewModel.class);
+        userNameText = findViewById(R.id.input_username);
+        nameText = findViewById(R.id.input_name);
+        emailText = findViewById(R.id.input_email);
+        passwordText = findViewById(R.id.input_password);
+        reEnterPasswordText = findViewById(R.id.input_reEnterPassword);
+        signupButton = findViewById(R.id.btn_signup);
+        loginLink = findViewById(R.id.link_login);
+
+        signupButton.setOnClickListener(v -> signup());
+
+        loginLink.setOnClickListener(v -> {
+            // Finish the registration screen and return to the Login activity
+            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+        });
+    }
+
+    public void signup() {
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        signupButton.setEnabled(false);
+
+        String username = userNameText.getText().toString();
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        loginViewModel.createUser(username, name, email, password);
+        onSignupSuccess();
+        //onSignupFailed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onSignupSuccess() {
+        signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+        overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String username = userNameText.getText().toString();
+        String name = nameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        String reEnterPassword = reEnterPasswordText.getText().toString();
+
+        if (username.isEmpty()) {
+            nameText.setError("Enter a Username!");
+            valid = false;
+        } else {
+            userNameText.setError(null);
+        }
+
+        if (name.isEmpty()) {
+            nameText.setError("Enter a name!");
+            valid = false;
+        } else {
+            nameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("Enter a valid email address!");
+            valid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            passwordText.setError("Password must be at least 6 characters long!");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 6 || !(reEnterPassword.equals(password))) {
+            reEnterPasswordText.setError("Passwords do not match!");
+            valid = false;
+        } else {
+            reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+}
